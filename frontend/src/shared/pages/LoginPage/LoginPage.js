b48a0716a445dfbcd6991c4da2d542fac37a3aea
Lloyd Ntim <lloyd.ntim@gmail.com>
2022-01-09 14:53:05 +0000

    Refactoring

--- frontend/src/shared/pages/LoginPage/LoginPage.js
+++ frontend/src/shared/pages/LoginPage/LoginPage.js
@@ -11,88 +11,88 @@ import { Message, Input, Button } from '../../components';
 import './LoginPage.scss';
 
 export const LOGIN = gql`
-	mutation Login($username: String, $password: String) {
-		login(username: $username, password: $password) {
-			token
-		}
-	}
+  mutation Login($username: String, $password: String) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
 `;
 
 const LoginPage = () => {
-	const { push } = useHistory();
-	const { t } = useTranslation();
-	const inputNames = ['username', 'password'];
-	const { formData, updateFormData, isFormValid } = useForm(inputNames);
-	const { username, password } = formData;
-	const [responseMessage, setResponseMessage] = useState('');
-	const [login, { loading }] = useMutation(LOGIN, {
-		onCompleted: (data) => {
-			/* eslint-disable no-undef */
-			localStorage.setItem('token', data.login.token);
-			push('/vocablists');
-		},
-		onError: (error) => {
-			// Sentry.captureException(error);
-			// console.log('Login: ', error);
-			setResponseMessage(error.message.split(':')[1].trim());
-		},
-	});
-	return (
-		<RootLayout type="guest">
-			<div className="login-page page">
-				<h1>{t('login_title')}</h1>
-				<form>
-					<Input
-						label={t('common_form_label_username')}
-						autocapitalize="off"
-						inputRef={username.ref}
-						required
-						autoComplete="username"
-						name={username.name}
-						placeholder={t('common_form_placeholder_username')}
-						value={username.value}
-						onChange={updateFormData}
-						onBlur={updateFormData}
-					/>
-					<Input
-						label={t('common_form_placeholder_password')}
-						autocapitalize="off"
-						inputRef={password.ref}
-						required
-						name={password.name}
-						type="password"
-						placeholder={t('common_form_label_password')}
-						value={password.value}
-						onChange={updateFormData}
-						onBlur={updateFormData}
-					/>
-					<Button
-						rank="primary"
-						disabled={!isFormValid}
-						text={t('common_button_login')}
-						onClick={() =>
-							login({
-								variables: {
-									username: username.value.toLowerCase(),
-									password: password.value,
-								},
-							})
-						}
-					/>
-				</form>
+  const { push } = useHistory();
+  const { t } = useTranslation();
+  const inputNames = ['username', 'password'];
+  const { formData, updateFormData, isFormValid } = useForm(inputNames);
+  const { username, password } = formData;
+  const [responseMessage, setResponseMessage] = useState('');
+  const [login, { loading }] = useMutation(LOGIN, {
+    onCompleted: (data) => {
+      /* eslint-disable no-undef */
+      localStorage.setItem('token', data.login.token);
+      push('/vocablists');
+    },
+    onError: (error) => {
+      // Sentry.captureException(error);
+      // console.log('Login: ', error);
+      setResponseMessage(error.message.split(':')[1].trim());
+    },
+  });
+  return (
+    <RootLayout type="guest">
+      <div className="login-page page">
+        <h1>{t('login_title')}</h1>
+        <form>
+          <Input
+            label={t('common_form_label_username')}
+            autocapitalize="off"
+            inputRef={username.ref}
+            required
+            autoComplete="username"
+            name={username.name}
+            placeholder={t('common_form_placeholder_username')}
+            value={username.value}
+            onChange={updateFormData}
+            onBlur={updateFormData}
+          />
+          <Input
+            label={t('common_form_placeholder_password')}
+            autocapitalize="off"
+            inputRef={password.ref}
+            required
+            name={password.name}
+            type="password"
+            placeholder={t('common_form_label_password')}
+            value={password.value}
+            onChange={updateFormData}
+            onBlur={updateFormData}
+          />
+          <Button
+            rank="primary"
+            disabled={!isFormValid}
+            text={t('common_button_login')}
+            onClick={() =>
+              login({
+                variables: {
+                  username: username.value.toLowerCase(),
+                  password: password.value,
+                },
+              })
+            }
+          />
+        </form>
 
-				<div className="link-group">
-					<Link to="/signup">{t('common_button_signup')}</Link>
-					<Link to="/forgot">{t('login_forgotPassword')}</Link>
-				</div>
+        <div className="link-group">
+          <Link to="/signup">{t('common_button_signup')}</Link>
+          <Link to="/forgot">{t('login_forgotPassword')}</Link>
+        </div>
 
-				{loading && (
-					<Message type="info" content={t('messages_info_loading')} />
-				)}
-				{responseMessage && <Message type="error" content={responseMessage} />}
-			</div>
-		</RootLayout>
-	);
+        {loading && (
+          <Message type="info" content={t('messages_info_loading')} />
+        )}
+        {responseMessage && <Message type="error" content={responseMessage} />}
+      </div>
+    </RootLayout>
+  );
 };
 
 export default LoginPage;
