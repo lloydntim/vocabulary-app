1997e57cfaadf305ff2e2a81391cb3ffb76c1de6
Lloyd Ntim <lloyd.ntim@gmail.com>
2022-01-09 14:21:21 +0000

    Refactoring

--- frontend/src/shared/pages/LoginPage/LoginPage.js
+++ frontend/src/shared/pages/LoginPage/LoginPage.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import gql from "graphql-tag";
-import { useMutation } from "@apollo/react-hooks";
-import { Link, useHistory } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { Link, useHistory } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 // import * as Sentry from '@sentry/browser';
-import { useForm } from "../../hooks";
-import { RootLayout } from "../../layouts";
-import { Message, Input, Button } from "../../components";
+import { useForm } from '../../hooks';
+import { RootLayout } from '../../layouts';
+import { Message, Input, Button } from '../../components';
 
-import "./LoginPage.scss";
+import './LoginPage.scss';
 
 export const LOGIN = gql`
 	mutation Login($username: String, $password: String) {
@@ -21,47 +21,47 @@ export const LOGIN = gql`
 const LoginPage = () => {
 	const { push } = useHistory();
 	const { t } = useTranslation();
-	const inputNames = ["username", "password"];
+	const inputNames = ['username', 'password'];
 	const { formData, updateFormData, isFormValid } = useForm(inputNames);
 	const { username, password } = formData;
-	const [responseMessage, setResponseMessage] = useState("");
+	const [responseMessage, setResponseMessage] = useState('');
 	const [login, { loading }] = useMutation(LOGIN, {
 		onCompleted: (data) => {
 			/* eslint-disable no-undef */
-			localStorage.setItem("token", data.login.token);
-			push("/vocablists");
+			localStorage.setItem('token', data.login.token);
+			push('/vocablists');
 		},
 		onError: (error) => {
 			// Sentry.captureException(error);
 			// console.log('Login: ', error);
-			setResponseMessage(error.message.split(":")[1].trim());
+			setResponseMessage(error.message.split(':')[1].trim());
 		},
 	});
 	return (
 		<RootLayout type="guest">
 			<div className="login-page page">
-				<h1>{t("login_title")}</h1>
+				<h1>{t('login_title')}</h1>
 				<form>
 					<Input
-						label={t("common_form_label_username")}
+						label={t('common_form_label_username')}
 						autocapitalize="off"
 						inputRef={username.ref}
 						required
 						autoComplete="username"
 						name={username.name}
-						placeholder={t("common_form_placeholder_username")}
+						placeholder={t('common_form_placeholder_username')}
 						value={username.value}
 						onChange={updateFormData}
 						onBlur={updateFormData}
 					/>
 					<Input
-						label={t("common_form_placeholder_password")}
+						label={t('common_form_placeholder_password')}
 						autocapitalize="off"
 						inputRef={password.ref}
 						required
 						name={password.name}
 						type="password"
-						placeholder={t("common_form_label_password")}
+						placeholder={t('common_form_label_password')}
 						value={password.value}
 						onChange={updateFormData}
 						onBlur={updateFormData}
@@ -69,7 +69,7 @@ const LoginPage = () => {
 					<Button
 						rank="primary"
 						disabled={!isFormValid}
-						text={t("common_button_login")}
+						text={t('common_button_login')}
 						onClick={() =>
 							login({
 								variables: {
@@ -82,12 +82,12 @@ const LoginPage = () => {
 				</form>
 
 				<div className="link-group">
-					<Link to="/signup">{t("common_button_signup")}</Link>
-					<Link to="/forgot">{t("login_forgotPassword")}</Link>
+					<Link to="/signup">{t('common_button_signup')}</Link>
+					<Link to="/forgot">{t('login_forgotPassword')}</Link>
 				</div>
 
 				{loading && (
-					<Message type="info" content={t("messages_info_loading")} />
+					<Message type="info" content={t('messages_info_loading')} />
 				)}
 				{responseMessage && <Message type="error" content={responseMessage} />}
 			</div>
